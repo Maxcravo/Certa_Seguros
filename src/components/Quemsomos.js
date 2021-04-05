78761e117ac369b7989aa4c451cbe13636019c87
Maxcravo <65576090+Maxcravo@users.noreply.github.com>
2021-04-05 13:43:36 -0300

    Update Quemsomos.js

--- src/components/Quemsomos.js
+++ src/components/Quemsomos.js
@@ -7,7 +7,7 @@ export default function Quem_somos() {
             <div className={Styles.div_title_quemsomos}>
                 <h1 className={Styles.title} id="title">Quem somos?</h1>
             </div>
-            <p className={Styles.text1}>Certa Corretoras de Seguros Ltda, é uma corretora consolidada no mercado segurador, contando com uma equipe especializada em excelência em atendimento a clientes.</p>
+            <p className={Styles.text1}>Certa Corretora de Seguros Ltda, é uma corretora consolidada no mercado segurador, contando com uma equipe especializada em excelência em atendimento a clientes.</p>
             <p className={Styles.text2}>Com ela, seus clientes não precisam preocupar-se com a data da renovação de seu seguro, com o andamento de algum sinistro que possa ocorrer eventualmente, pois temos equipes que acompanham todos esses eventos de perto e de modo personalizado, permitindo uma aproximação entre nossos clientes e a companhia de seguros.</p>
             <p className={Styles.text4}>Com a Certa Corretora de Seguros você estará protegido e terá todo o suporte de nossa consultoria especializada com agilidade e clareza. Conheça nossos serviços, faça um seguro conosco.</p>
             <div className={Styles.div_title2_quemsomos}>
